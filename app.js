fb8a7cc9e1064f9671312991078909d990cd2964
Giulia R <84285314+RealGiulia@users.noreply.github.com>
2021-09-11 23:44:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World, here we are again!'));
+app.get('/', (req, res) => res.send('This is my first appliction using AWS resources!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
